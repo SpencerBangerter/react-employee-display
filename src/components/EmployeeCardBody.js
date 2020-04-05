a7c1a3850b617763329a7d157e74d424b9863bd9
Spencer Bangterer <bangerter.spencer@gmail.com>
2020-04-05 15:20:25 -0600

    Filter State working. Need to wire up a UI filter and update filters state function

--- src/components/EmployeeCardBody.js
+++ src/components/EmployeeCardBody.js
@@ -1,19 +1,35 @@
 import React from "react";
 
 function EmployeeCard(props) {
+  let renderedEmployees = props.employees
+  if (props.filters.currentlyEmployed) {
+    renderedEmployees = props.employees.filter(employee => employee.currentlyEmployed);
+  }
+  if (props.filters.department) {
+    renderedEmployees = props.employees.filter(employee => employee.department === props.filters.department);
+  }
+  if (props.filters.title) {
+    renderedEmployees = props.employees.filter(employee => employee.title === props.filters.title);
+  }
+
   return (
-    <div className="card">
-      <div className="content">
-         <div>
-            <strong>Name:</strong> {props.name}
-          </div>
-         <div>
-            <strong>Department:</strong> {props.department}
-          </div>
-         <div>
-            <strong>Title:</strong> {props.title}
-          </div>
-      </div>
+    <div>
+      {renderedEmployees.map(employee => (
+        <div className="card" key={employee.id}>
+        <div className="content">
+        <div>
+           <strong>Name:</strong> {employee.name}
+         </div>
+        <div>
+           <strong>Department:</strong> {employee.department}
+         </div>
+        <div>
+           <strong>Title:</strong> {employee.title}
+         </div>
+     </div>
+     </div>
+
+      ))}
     </div>
   );
 }
--- src/components/EmployeeContainer.js
+++ src/components/EmployeeContainer.js
@@ -7,7 +7,22 @@ import EmployeeCardBody from "./EmployeeCardBody";
 import employees from "../employees.json";
 
 class EmployeeContainer extends Component {
-  state = {};
+  state = {
+    employees,
+    filters: {
+      currentlyEmployed: true,
+      department: "Engineering",
+      title: 'Senior Developer',
+    }
+  };
+
+  // currentlyEmployed = x => {
+  //   if (!this.props.filters.currentlyEmployed) {
+  //     console.log(this.props.filters.currentlyEmployed)
+  //     // const employees = this.state.employees.filter(employee => employee.currentlyEmployed === true)
+  //     // this.setState({ employees });
+  //   }
+  // }
 
   //render base component
   render() {
@@ -16,15 +31,10 @@ class EmployeeContainer extends Component {
         <Row>
           <Col size="md-5">
             <Card heading="Employees">
-              {employees.map((employee) => (
-                <EmployeeCardBody
-                  id={employee.id}
-                  key={employee.id}
-                  name={employee.name}
-                  title={employee.title}
-                  department={employee.department}
-                />
-              ))}
+              <EmployeeCardBody 
+              employees = {employees}
+              filters = {this.state.filters}
+              />
             </Card>
           </Col>
           <Col size="md-6">
@@ -37,3 +47,14 @@ class EmployeeContainer extends Component {
 }
 
 export default EmployeeContainer;
+// {this.state.employees.map((employee) => (
+//   <EmployeeCardBody 
+//     id={employee.id}
+//     key={employee.id}
+//     name={employee.name}
+//     title={employee.title}
+//     department={employee.department}
+//     filters={this.state.filters}
+//   />
+// ))
+// }
